83f0ad010fffd2d59c37484be4719bdcb95ddb3e
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2025-08-15 03:21:23 +0200

    added enemy template support and api

---
+++ Enemies/EnemiesAPI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Gatekeeper.Enemy.Base.Data;
+using Gatekeeper.Enemy.SpawnTemplates;
+using Gatekeeper.General.Predictor;
+using Gatekeeper.Infrastructure.Providers.InfoProviders;
+using Gatekeeper.Utility.SceneHelper;
+using RNGNeeds;
+using UnityEngine;
+
+namespace GKAPI.Enemies;
+
+public class EnemiesAPI
+{
+    private readonly List<KnowingEnemySpawnTemplate> _templates = [];
+    
+    public static EnemiesAPI Instance { get; } = new();
+
+    private EnemiesAPI()
+    {
+        EventHandler.Init += RegisterEnemies;
+    }
+
+    public KnowingEnemySpawnTemplate AddKnowingTemplate(GameDifficulty difficulties, bool forBossFight, List<SceneDesignation> scenes, Vector2Int locationNumberRange, ProbabilityList<EnemyCharacterInfo> enemyProbabilityList, string name = "", bool ignore = false)
+    {
+        var template = ScriptableObject.CreateInstance<KnowingEnemySpawnTemplate>();
+        template.name = name;
+        template.difficulties = difficulties;
+        template.ForBossFight = forBossFight;
+        template.Ignore = ignore;
+        template.scenes = new Il2CppSystem.Collections.Generic.List<SceneDesignation>();
+        foreach (var scene in scenes)
+        {
+            template.scenes.Add(scene);
+        }
+        template.locationNumberRange = locationNumberRange;
+        template.ProbabilityListOfEnemy = enemyProbabilityList;
+        _templates.Add(template);
+        return template;
+    }
+
+    private void RegisterEnemies()
+    {
+        Plugin.Log.LogInfo("Registering enemies");
+
+        foreach (var template in _templates)
+        {
+            DatabaseInfoProvider.Enemies.AllKnowingEnemySpawnTemplates.Add(template);
+        }
+        Plugin.Log.LogInfo($"Added {_templates.Count} knowing templates");
+        
+        Plugin.Log.LogInfo("Registered enemies");
+    }
+}
---
+++ Enemies/TemplateHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Gatekeeper.Enemy.Base.Data;
+using Gatekeeper.Infrastructure.Providers.InfoProviders;
+using Gatekeeper.PoolScripts;
+using Gatekeeper.Utility.SceneHelper;
+using RNGNeeds;
+
+namespace GKAPI.Enemies;
+
+public class TemplateHelper
+{
+    public static List<SceneDesignation> AuroraScenes { get; } = [
+        SceneDesignation.AuroraAltarOfTheAwakened,
+        SceneDesignation.AuroraEdgeOfTheContinent,
+        SceneDesignation.AuroraLandOfTheAncients,
+        SceneDesignation.AuroraRunewood,
+    ];
+    public static List<SceneDesignation> CelestiumScenes { get; } = [
+        SceneDesignation.CelestiumArcaneOrbitals,
+        SceneDesignation.CelestiumEnshrinedExpanse,
+        SceneDesignation.CelestiumEtherealBridge,
+        SceneDesignation.CelestiumTheConfluxHall,
+    ];
+    public static List<SceneDesignation> PurgatoryScenes { get; } = [
+        SceneDesignation.Purgatory17AbodeOfAshes,
+        SceneDesignation.Purgatory17BridgeOfTheDamned,
+        SceneDesignation.Purgatory17HauntingEnclave,
+        SceneDesignation.Purgatory17WellOfSouls,
+    ];
+    public static List<SceneDesignation> AriduneScenes { get; } = [
+        SceneDesignation.AriduneBarrenBasin,
+        SceneDesignation.AriduneCursedCrossing,
+        SceneDesignation.AriduneOasis,
+        SceneDesignation.AriduneThePit,
+    ];
+    public static List<SceneDesignation> PaliumScenes { get; } = [
+        SceneDesignation.PaliumBroodsCrest,
+        SceneDesignation.PaliumEldenTree,
+        SceneDesignation.PaliumForgottenDepths,
+        SceneDesignation.PaliumTheMaw,
+    ];
+    
+    //TODO move to general helper and use generics
+    public class ProbListBuilder
+    {
+        private ProbabilityList<EnemyCharacterInfo> _list = new();
+
+        public ProbListBuilder AddItem(PoolItemID id, float probability)
+        {
+            _list.AddItem(DatabaseInfoProvider.Enemies.GetCharacterInfoByPoolID(id), probability);
+            return this;
+        }
+
+        public ProbabilityList<EnemyCharacterInfo> Build()
+        {
+            float sum = 0;
+            foreach (var item in _list.ProbabilityItems)
+            {
+                sum += item.Probability;
+            }
+            
+            if (Math.Abs(sum - 1.0) > 0.001)
+                Plugin.Log.LogWarning("Probabilities of current Template does not add up to 1");
+            
+            return _list;
+        }
+    }
+}
